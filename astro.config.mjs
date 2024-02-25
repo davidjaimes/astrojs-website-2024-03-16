2433010db893e105d8148708c257b562e7ddee17
David Jaimes <dj@davidjaimes.com>
2024-02-25 13:52:53 -0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  sitemap: true,
+  site: "https://xomil.com",
+  
+  integrations: [tailwind({
+    config: {
+      applyBaseStyles: true
+    }
+  }), react()],
+  
+  markdown: {
+    shikiConfig: {
+      theme: 'github-dark-dimmed',
+      wrap: false
+    },
+  },
+});
